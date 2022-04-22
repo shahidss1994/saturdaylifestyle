52d5a7ee762d5d110f0c4e3da795bc3be341a49e
shahidss1994 <shahidss1994@gmail.com>
2022-04-23 05:24:46 +0530

    architectural fix

---
+++ app/src/main/java/com/shock/saturdaylifestyle/ui/base/fragment/BottomSheetBaseFragment.kt
@@ -0,0 +1,33 @@
+package com.shock.saturdaylifestyle.ui.base.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BottomSheetBaseFragment<out T : ViewDataBinding>(@LayoutRes private val screenLayoutId: Int) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: T
+    protected fun binding() = binding
+
+    abstract fun listenChannel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, screenLayoutId, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listenChannel()
+    }
+
+}
